976cf55817a3b21590c7ecff7ed87f9e2e6d9a14
Daisuke Hirata <daisukihirata@gmail.com>
2017-09-09 22:55:01 +0900

    add placeholder and snap feature

--- Assets/DragAndDrop.cs
+++ Assets/DragAndDrop.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class DragAndDrop : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Update is called once per frame
+    void Update () {
+
+    }
 
     public void Drag() {
         GameObject.Find("Image").transform.position = Input.mousePosition;
         print("Dragging" + gameObject.name);
+    }
+
+    public void Drop() {
+        GameObject ph1 = GameObject.Find("PlaceHolder1");
+        GameObject img = GameObject.Find("Image");
+
+        float distance = Vector3.Distance(ph1.transform.position, img.transform.position);
+
+        if (distance < 50) {
+            img.transform.position = ph1.transform.position;
+        }
     }
 }
